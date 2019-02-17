0e28f0f66ae5cde027451de8622a59cdf32dca3a
景青山 <285138556@qq.com>
2019-02-17 18:35:11 +0800

    问候世界

---
+++ src/main/java/cn/shan/hello.java
@@ -0,0 +1,18 @@
+package cn.shan;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println(
+                "helloworld,HLPE"
+        );
+        System.out.println(
+                "helloworld,HLPE"
+        );
+        System.out.println(
+                "helloworld,HLPE"
+        );
+        System.out.println(
+                "helloworld,HLPE"
+        );
+    }
+}
